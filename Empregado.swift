0e47c444c9106b427a3f2e1e4d42e889929143fa
João Maniaudet <jmmegale@gmail.com>
2024-12-25 18:57:01 -0300

    Create Empregado.swift

---
+++ Empregado.swift
@@ -0,0 +1,17 @@
+class Empregado {
+  var nome: String
+  var salario: Double
+
+  init(nome: String, salario: Double) {
+    self.nome = nome
+    self.salario = salario
+  }
+}
+
+class Gerente: Empregado {
+  var departamento: String
+  init(nome: String, salario: Double, departamento: String) {
+    self.departamento = departamento
+    super.init(nome: nome, salario: salario)
+  }
+}
